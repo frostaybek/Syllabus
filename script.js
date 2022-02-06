38644e004ae5eb74f4ec636667d94a3d4ef72b51
Ayaz Aybek <87331452+frostaybek@users.noreply.github.com>
2022-02-06 09:50:43 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function math(){
+    alert('Matematik Dersi (6 Saat)');
+}
+
+function tr(){
+    alert('Edebiyat Dersi (5 Saat)');
+}
+
+function ger(){
+    alert('Almanca Dersi (2 Saat)');
+}
+
+function eng(){
+    alert('İngilizce Dersi (4 Saat)');
+}
